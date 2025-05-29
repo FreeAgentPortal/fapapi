5b39897be0da28ff47caac97be07efd0c9da1c52
Austin Howard <cnfishead@hotmail.com>
2025-05-29 18:42:19 -0400

    added team and routes

--- src/route/v1/apiV1Routes.ts
+++ src/route/v1/apiV1Routes.ts
@@ -1,10 +1,12 @@
 import express from 'express'; 
 import authRoutes from '../../modules/auth/route/authRoutes';
 import athleteRoutes from '../../modules/athlete/route/index';
+import teamRoutes from '../../modules/team/route/index';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/athlete', athleteRoutes); 
+router.use('/team', teamRoutes); 
 
 export default router;
