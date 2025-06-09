e2b582660a12fe927732e5de711d679c29b3452f
Austin Howard <cnfishead@hotmail.com>
2025-06-09 14:53:23 -0400

    removed bad console log from auth middleware

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -33,8 +33,7 @@ export class AuthMiddleware {
     try {
       const token = req.headers.authorization!.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
-      req.user = await User.findById(decoded.userId).select('-password');
-      console.log(req.user);
+      req.user = await User.findById(decoded.userId).select('-password'); 
       if (!req.user) {
         return res.status(401).json({ message: 'User not found.' });
       }
